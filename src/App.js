b41a4839e12a1122823b7fbfbf594cbc2aa9f0eb
Tom Stockwood <tomstockwood@hotmail.co.uk>
2019-12-09 11:28:28 +0000

    Created q increment function, added to buttons

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ const value1 = Math.floor(Math.random() * 100);
 const value2 = Math.floor(Math.random() * 100);
 const value3 = Math.floor(Math.random() * 100);
 const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+//const numQuestions = 0;
+//const numCorrect = 0;
 
 class App extends Component {
+  state = {
+    numQuestions : 0,
+    numCorrect : 0
+  }
+  
+increaseNumQuestions = () => {
+  this.setState((currentState) => ({
+    numQuestions : currentState.numQuestions + 1
+  }))
+}
+
   render() {
     return (
       <div className="App">
@@ -22,12 +33,13 @@ class App extends Component {
           <div className="equation">
             <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick = {() => this.increaseNumQuestions()} 
+          >True</button>
+          <button onClick = {() => this.increaseNumQuestions()}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
-          Hello world
+          Hello world!!?
         </div>
       </div>
     );
